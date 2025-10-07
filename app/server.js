5fe685f978528e83248879c700be4c97478a585a
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-10-07 12:57:51 +0100

    Main page picture configurable.

--- app/server.js
+++ app/server.js
@@ -18,7 +18,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/profile-picture", function (req, res) {
-  let img = fs.readFileSync(path.resolve("./images/profile-1.jpg"));
+  let img = fs.readFileSync(path.resolve(`./images/${process.env.IMAGE}.jpg`));
   res.writeHead(200, { "Content-Type": "image/jpg" });
   res.end(img, "binary");
 });
